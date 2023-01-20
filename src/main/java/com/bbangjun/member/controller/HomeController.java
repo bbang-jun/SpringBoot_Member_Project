cf46e31f63760eb554b61738fcd8b9824c0e32c4
bbangjun <lucid9dawn@gmail.com>
2023-01-20 21:07:18 +0900

    feat(HomeController): Add 기본 페이지 요청 메서드

---
+++ src/main/java/com/bbangjun/member/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.bbangjun.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    // 기본 페이지 요청 메서드
+    @GetMapping("/")
+    public String index(){
+        return "index"; // resources/templates 폴더의 index.html을 반환(view resolver)
+    }
+}
