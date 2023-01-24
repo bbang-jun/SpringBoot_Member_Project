97d6a75a7ca6fed2ad1727f2ed8fb43e92fee9d4
bbangjun <lucid9dawn@gmail.com>
2023-01-25 00:15:25 +0900

    docs(build.gradle): Add jpa and mysql

--- src/main/java/com/bbangjun/member/controller/MemberController.java
+++ src/main/java/com/bbangjun/member/controller/MemberController.java
@@ -1,6 +1,8 @@
 package com.bbangjun.member.controller;
 
 import com.bbangjun.member.dto.MemberDto;
+import com.bbangjun.member.service.MemberSerice;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -8,7 +10,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequiredArgsConstructor
 public class MemberController {
+    // 생성자 주입
+    private final MemberSerice memberSerice;
+
     // 회원가입 페이지 출력 요청
     @GetMapping("/member/save")
     public String saveForm(){
@@ -19,6 +25,7 @@ public class MemberController {
     public String save(@ModelAttribute MemberDto memberDto){
         System.out.println("MemberController.save");
         System.out.println("memberDto = " + memberDto);
+        memberSerice.save(memberDto);
         return "index"; // index.html 화면으로 이동
     }
 }
--- src/main/java/com/bbangjun/member/service/MemberSerice.java
+++ src/main/java/com/bbangjun/member/service/MemberSerice.java
@@ -1,7 +1,13 @@
 package com.bbangjun.member.service;
 
+import com.bbangjun.member.dto.MemberDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MemberSerice {
+    private final MemberRepository memberRepository;
+    public void save(MemberDto memberDto) {
+    }
 }
